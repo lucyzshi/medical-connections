f01fc76fb78a2db2b8720c7a7e786c902d4b88f9
lucyzshi <shi.lucyz@gmail.com>
2025-09-23 08:46:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnc5HI3Qti60AXXDCpL9B-YfBQNYW4MXM",
+  authDomain: "leaderboard-7580a.firebaseapp.com",
+  databaseURL: "https://leaderboard-7580a-default-rtdb.firebaseio.com",
+  projectId: "leaderboard-7580a",
+  storageBucket: "leaderboard-7580a.appspot.com",
+  messagingSenderId: "1065369349992",
+  appId: "1:1065369349992:web:f8cc82b10ada7d286730dd",
+  measurementId: "G-QT1C8X36P8"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
 let groups = {};
 let selectedTiles = [];
 let solvedGroups = [];
@@ -278,23 +296,6 @@ function shuffleRemainingTiles() {
 
 
 
-import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-database.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBnc5HI3Qti60AXXDCpL9B-YfBQNYW4MXM",
-  authDomain: "leaderboard-7580a.firebaseapp.com",
-  databaseURL: "https://leaderboard-7580a-default-rtdb.firebaseio.com",
-  projectId: "leaderboard-7580a",
-  storageBucket: "leaderboard-7580a.appspot.com",
-  messagingSenderId: "1065369349992",
-  appId: "1:1065369349992:web:f8cc82b10ada7d286730dd",
-  measurementId: "G-QT1C8X36P8"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
 function saveWinStreak(name, streak) {
   const newRef = push(ref(db, 'leaderboard'));
   set(newRef, {
@@ -323,10 +324,6 @@ if (!sessionStorage.getItem("visited")) {
   });
 }
 
-import { getDatabase, ref, push, set } 
-  from "https://www.gstatic.com/firebasejs/11.8.1/firebase-database.js";
-
-const db = getDatabase(app);
 
 function saveComment(text) {
   const commentsRef = ref(db, "comments");
